9d6a005de4cd425804c878f2a39b48cd18ecebfb
Luong Quang Manh <luongquangmanh85@gmail.com>
2018-11-30 15:43:31 +0700

    Clean up

--- src/commands/hello.js
+++ src/commands/hello.js
@@ -1,20 +1,18 @@
-const {Command, flags} = require('@oclif/command')
+const { Command, flags } = require('@oclif/command')
+
 
 class HelloCommand extends Command {
   async run() {
-    const {flags} = this.parse(HelloCommand)
-    const name = flags.name || 'world'
-    this.log(`hello ${name} from ./src/commands/hello.js`)
+    const { args, flags } = this.parse(HelloCommand)
   }
 }
 
-HelloCommand.description = `Describe the command here
-...
-Extra documentation goes here
+HelloCommand.description = `InfluxDB benchmarks
 `
 
 HelloCommand.flags = {
-  name: flags.string({char: 'n', description: 'name to print'}),
+  name: flags.string({ char: 'n', description: 'name to print' }),
 }
+
 
 module.exports = HelloCommand
